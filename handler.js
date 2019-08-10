1e22c085c393183d15b50424c21b57848fe0af09
fourside <fourside@gmail.com>
2019-08-10 11:25:18 +0900

    refactor: remove redundancy code

--- handler.js
+++
@@ -1,9 +0,0 @@
-exports.createHandler = (method) => {
-  return new Handler(method);
-}
-
-const Handler = function(method) {
-  this.process = function(req, res) {
-    return method.apply(this, [req, res]);
-  }
-}
--- router.js
+++ router.js
@@ -1,15 +1,21 @@
-const handlerFactory = require('./handler');
 const fs = require('fs');
 const parser = require('url');
 const handlers = {};
 
+const Handler = function(method) {
+  this.process = (req, res) => {
+    return method.apply(this, [req, res]);
+  }
+}
+
 exports.register = (url, method) => {
-  handlers[url] = handlerFactory.createHandler(method);
+  handlers[url] = new Handler(method);
 }
 
 exports.route = (req) => {
   const url = parser.parse(req.url, true);
-  const handler = handlers[url.pathname] || this.missing(req);
+  const query = url.search || "";
+  const handler = handlers[url.pathname + query] || this.missing(req);
   return handler;
 }
 
@@ -20,14 +26,14 @@ exports.missing = (req) => {
   try {    
     const data = fs.readFileSync(path);
     const mime = req.headers.accepts || 'text/html'
-    return handlerFactory.createHandler((req, res) => {
+    return new Handler((req, res) => {
       res.writeHead(200, {'Content-Type': mime});
       res.write(data);
       res.end();
     });        
   } catch (e) { 
     console.log(e);
-    return handlerFactory.createHandler((req, res) => {
+    return new Handler((req, res) => {
       res.writeHead(404, {'Content-Type': 'text/plain'});
       res.write("No route registered for " + url.pathname);
       res.end();
