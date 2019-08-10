4a0a4d8c3842f8dc33df9d01ae1020122630be57
fourside <fourside@gmail.com>
2019-08-10 17:41:40 +0900

    improve logging message

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ const router = require('./router');
 const server = http.createServer((req, res) => {
   const handler = router.route(req);
   handler.process(req, res);
+  console.log("[%s] %s %s %s", (new Date()).toLocaleTimeString(), req.method, req.url, res.statusCode)
 });
 
-// Start it up
 server.listen(8000);
-console.log('Server running');
+console.log("[%s] %s", (new Date()).toLocaleTimeString(), 'Server running.. listening 8000');
+
